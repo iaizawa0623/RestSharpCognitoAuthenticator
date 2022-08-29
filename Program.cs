6c4207a744b0fbaa480ec132ec3533fb5c61341d
Ibuki Aizawa <83331063+iaizawa0623@users.noreply.github.com>
2022-08-29 11:14:22 +0900

    例を簡素化

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
+/* 使用例 */
 using RestSharp;
 using RestSharpCognitoAuthenticator;
 
-#region // 外部ファイルで設定できる項目
-//  認証サーバーのURL
 const string idpUrl = "https://cognito-idp.ap-northeast-1.amazonaws.com/";
-const string clientId = "2qiutf2l0qlq5agfrbb0di47kj";
-const string username = "developer";
-const string password = "IvDF0dZ_";
+const string clientId = "<clientId>";
+const string username = "<username>";
+const string password = "<passwerd>";
 //  APIサーバーのURL
-const string apiUrl = "https://api.bus-de-odekake.od-one.net";
-#endregion
+const string apiUrl = "<apiUrl>";
 
 // APIクライアント
 var client = new RestClient(apiUrl)
@@ -22,23 +20,7 @@ var client = new RestClient(apiUrl)
     )
 };
 
-//	チケット数リクエスト
-var issuerRequest = new RestRequest($"/publishers/{username}/{DateTime.Today:yyyyMMdd}");
-var issuerResponse = await client.GetAsync(issuerRequest);
-Console.WriteLine(issuerResponse?.Content);
-
-//	チケット発行リクエスト
-var ticketNo = 1;
-var serialNo = $"{username}{DateTime.Today:yyyyMMdd}{ticketNo}";
-var body = $@"{{
-	""ticketType"": ""digital"",
-	""survey"": {{
-		""性別"": ""女性"",
-		""年齢"": ""40代""
-	}}
-}}";
-var ticketRequet = new RestRequest($"/tickets/{serialNo}")
-    .AddJsonBody(body);
-var ticketResponse = await client.PostAsync(ticketRequet);
-Console.WriteLine(ticketResponse?.Content);
-
+//	Get /hello リクエスト
+var helloRequest = new RestRequest($"/hello");
+var helloResponse = await client.GetAsync(helloRequest);
+Console.WriteLine(helloResponse?.Content);
